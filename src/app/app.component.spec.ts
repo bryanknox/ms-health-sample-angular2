efc3518eff061d82693b31a04e3c154c21a67228
bryanknox <bryan@knoxbits.com>
2016-11-26 17:01:13 -0800

    Upgraded to angular-cli@1.0.0-beta.21
    and Angular 2.2.1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: BkNg2MsHealth', () => {
+describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
